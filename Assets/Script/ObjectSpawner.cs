0b2eefd41b033b9ffcc90f8d40acbfa8f251e25a
pournesh <pournesh96@gmail.com>
2023-01-20 23:27:44 +0530

    Simple AR Placement App
    Tap to place

---
+++ Assets/Script/ObjectSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    bool object_spawned;
+    private Placementndicator placementndicator;
+    private GameObject obj;
+    public GameObject visual;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        object_spawned = false;
+        placementndicator = FindObjectOfType<Placementndicator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount >0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            if(!object_spawned)
+            {
+                obj = Instantiate(objectToSpawn, placementndicator.transform.position, placementndicator.transform.rotation);
+                object_spawned = true;
+                visual.SetActive(false);
+            }
+            else
+            {
+                obj.transform.position = placementndicator.transform.position;
+            }
+            //GameObject obj = Instantiate(objectToSpawn, placementndicator.transform.position, placementndicator.transform.rotation);
+            
+        }
+    }
+}
